2222dd746301b82503e614bf951762140011010c
Fernando Silva <fernandosilvadj@hotmail.com>
2021-11-26 16:52:52 -0300

    feat: validating account

--- modulo01/src/index.js
+++ modulo01/src/index.js
@@ -19,6 +19,17 @@ app.post('/account', (req, res) => {
   return res.status(201).send()
 })
 
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params
+  const customer = customers.find((customer) => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(404).json({ error: 'Customer not found' })
+  }
+
+  return res.json(customer.statement)
+})
+
 app.listen(3333, () => {
   console.log('Server is running 🚀')
 })
